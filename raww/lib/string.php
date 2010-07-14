e460c258e2a7600e58b9657b47c2258403f35390
Artur Heinze <artur@d-xp.com>
2010-07-15 00:48:53 +0200

    String::slugify() and String::time2str() added

--- raww/lib/string.php
+++ raww/lib/string.php
@@ -30,7 +30,7 @@ class String{
   
   public static function strlen($str){
 		return mb_strlen($str);
-	}
+  }
   
   public static function strpos($str, $search, $offset = false){
     if (strlen($str) && strlen($search)){
@@ -38,19 +38,66 @@ class String{
 		}else{
       return false;
     }
-	}
+  }
   
   public static function substr($str, $offset, $length = false){
     return ($length===false) ? mb_substr($str, $offset) : mb_substr($str, $offset, $length);
-	}
+  }
   
   public static function strtolower($str){
 		return mb_strtolower($str);
-	}
+  }
   
   public static function strtoupper($str){
 		return mb_strtoupper($str);
-	}
+  }
+  
+  public static function time2str($ts){
+        
+    if(!ctype_digit($ts)) $ts = strtotime($ts);
+
+    $diff = time() - $ts;
+        
+    if($diff == 0) return 'now';
+    
+    if($diff > 0) {
+        $day_diff = floor($diff / 86400);
+        if($day_diff == 0){
+            if($diff < 60) return 'just now';
+            if($diff < 120) return '1 minute ago';
+            if($diff < 3600) return floor($diff / 60) . ' minutes ago';
+            if($diff < 7200) return '1 hour ago';
+            if($diff < 86400) return floor($diff / 3600) . ' hours ago';
+        }
+        if($day_diff == 1) return 'Yesterday';
+        if($day_diff < 7) return $day_diff . ' days ago';
+        if($day_diff < 31) return ceil($day_diff / 7) . ' weeks ago';
+        if($day_diff < 60) return 'last month';
+        return date('F Y', $ts);
+    } else {
+        $diff = abs($diff);
+        $day_diff = floor($diff / 86400);
+        if($day_diff == 0){
+            if($diff < 120) return 'in a minute';
+            if($diff < 3600) return 'in ' . floor($diff / 60) . ' minutes';
+            if($diff < 7200) return 'in an hour';
+            if($diff < 86400) return 'in ' . floor($diff / 3600) . ' hours';
+        }
+        if($day_diff == 1) return 'Tomorrow';
+        if($day_diff < 4) return date('l', $ts);
+        if($day_diff < 7 + (7 - date('w'))) return 'next week';
+        if(ceil($day_diff / 7) < 4) return 'in ' . ceil($day_diff / 7) . ' weeks';
+        if(date('n', $ts) == date('n') + 1) return 'next month';
+        return date('F Y', $ts);
+    }
+  }
+  
+  public static function slugify($str){
+    $str = preg_replace('/[^a-zA-Z0-9 -]/', '', $str);
+    $str = strtolower(str_replace(' ', '-', trim($str)));
+    $str = preg_replace('/-+/', '-', $str);
+    return $str;
+  }
 
 }
 ?>
